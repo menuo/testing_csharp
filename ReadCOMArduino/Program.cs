96814dd7a0deca2bfbd55c38556f552960a7ba9d
menuo <menuo@BENDER>
2016-04-17 18:21:56 +0200

    Add project files.

---
+++ ReadCOMArduino/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace ReadCOMArduino
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            SerialPort mySerialPort = new SerialPort("COM3");
+
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            mySerialPort.Open();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine();
+            Console.ReadKey();
+            mySerialPort.Close();
+        }
+
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Debug.Print("Data Received:");
+            Debug.Print(indata);
+        }
+    }
+}
